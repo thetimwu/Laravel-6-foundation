604ec998aa2fa88f24a635ca5023309176a70d8b
tim wu <timwuuk@gmail.com>
2020-07-01 18:16:47 +0100

    php generator

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,37 @@
 |
 */
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\LazyCollection;
+use SebastianBergmann\Environment\Console;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/amy',  function () {
-    return view('amy.index');
+Route::get('/lazy',  function () {
+    // $collection = Collection::times(100)->map(function ($num) {
+    //     return pow(2, $num);
+    // })->all();
+
+    $collection = LazyCollection::times(100)->map(function ($num) {
+        return pow(2, $num);
+    })->all();
+
+    dump($collection);
 });
 
+// use 'yield' rather than 'return'
+Route::get('/generator', function () {
+    function happyFunction($strings)
+    {
+        foreach ($strings as $s) {
+            yield $s;
+        }
+    }
+
+    // return happyFunction()->current();
+    foreach (happyFunction([1, 2, 3, 4, 5, 6]) as $result) {
+        dump($result);
+    }
+});
